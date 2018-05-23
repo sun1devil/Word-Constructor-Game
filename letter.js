d024919c1e663f03c43ddefaf4752865b2b3ff8d
sun1devil <codejmc11@gmail.com>
2018-05-22 17:28:00 -0700

    make initial files and set first structure

---
+++ letter.js
@@ -0,0 +1,27 @@
+
+
+var Letter = function(letters, boolean, guessed, updateboolean){
+  this.letters = [];
+  this.guessed = true;
+  
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = Letter;
---
+++ word.js
@@ -0,0 +1,20 @@
+var Word =require("./letter.js");
+
+
+var Word = function(letterarray, functionwordstring, functionguess){
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = Word;
